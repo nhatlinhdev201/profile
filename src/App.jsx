19ebbc4f4f8a8b613ba2e5385fcfae00c8d01860
nhatlinh <nhatlinhdev201@gmail.com>
2024-07-16 09:16:43 +0700

    update/SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 import Banner from "./components/banner/Banner";
 import Contact from "./components/contact/Contact";
 // import Footer from "./components/footer/Footer";
@@ -8,14 +8,14 @@ import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <>
-      <Helmet>
+      {/* <Helmet>
         <title>Nhật Linh profile</title>
         <meta name="description" content="Nhật Linh - Lập trình viên Front End. Ghé thăm trang web giới thiệu bản thân của Nhật Linh !" />
         <meta name="keywords" content="Nhật Linh, React Vite, Profile, Portfolio" />
         <meta property="og:title" content="Trang chủ - Nhật Linh" />
         <meta property="og:description" content="Nhật Linh - Lập trình viên Front End. Ghé thăm trang web giới thiệu bản thân của Nhật Linh !" />
         <meta property="og:type" content="website" />
-      </Helmet>
+      </Helmet> */}
       <div className="w-full h-auto bg-bodyColor text-lightText px-4">
         <Navbar />
         <div className="max-w-screen-xl mx-auto">
